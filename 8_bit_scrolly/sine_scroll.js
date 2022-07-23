2ea06827e5fde009c5cbd35d1f71136c86f229b6
fr1550n <fr1550n@users.noreply.github.com>
2022-07-23 20:19:31 +0100

    - makes the scroller occupy the full screen - improved logging - scroll speed now controlled by a variable

--- 8_bit_scrolly/sine_scroll.js
+++ 8_bit_scrolly/sine_scroll.js
@@ -1,23 +1,34 @@
-const text = 'old school sine scroller    another cool javascript tutorial';
-const fontWidth = 54;
-const fontHeight = 71;
-const letters = 12;
+const text            = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. old school sine scroller another cool javascript tutorial';
+const fontWidth       = 54;
+const fontHeight      = 71;
+const letters 	      = parseInt(window.innerWidth / fontWidth);
 const letterEntryXpos = (letters - 1) * fontWidth;
+const xScrollSpeed    = 5;
 
-let x = [];
-let char = [];
-let wiggle = 0;
-let counter = 0;
+const debug = true;
+
+let x        = [];
+let char     = [];
+let wiggle   = 0;
+let counter  = 0;
 let position = letters;
-let bitmap = new Image();
-bitmap.src = 'font.png';
-let canvas = document.getElementById('myCanvas');
-let context = canvas.getContext('2d');
+let bitmap   = new Image();
+bitmap.src   = 'font.png';
+let canvas   = document.getElementById('myCanvas');
+canvas.width = window.innerWidth;
+let context  = canvas.getContext('2d');
 
+function log(s) {
+    if (debug) {
+        console.log(s);		    
+    }
+}
+
+log(`initialising the x${letters} slots: `);
 for (let n = 0; n < letters; n++) {
     char[n] = text.charCodeAt(n) - 97;
     x[n] = n * fontWidth;
-    console.log(`n=${n} alphabet character at index: ${text.charCodeAt(n)-97} ${String.fromCharCode(char[n]+97)}`);
+    log(`slot: ${n} will hold alphabetic character at index: ${text.charCodeAt(n)-97} which is: ${String.fromCharCode(char[n]+97)}`);
 }
 window.requestAnimationFrame(scroll);
 
@@ -25,10 +36,10 @@ window.requestAnimationFrame(scroll);
 function scroll() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     for (let n = 0; n < letters; n++) {
-        console.log(`${new Date().toLocaleString('en-GB', { timeZone: 'UTC' })}: n=${n} position for character: ${String.fromCharCode(char[n]+97)} = ${x[n]}`);	  	  
+        log(`${new Date().toLocaleString('en-GB', { timeZone: 'UTC' })}: n=${n} position for character: ${String.fromCharCode(char[n]+97)} = ${x[n]}`);	  	  
 	let y = 100; //+ wiggle * Math.sin(n + counter / 6.28);
         context.drawImage(bitmap, char[n] * fontWidth, 0, fontWidth, fontHeight, x[n], y, fontWidth, fontHeight);
-         x[n]--;
+         x[n]-=xScrollSpeed;
          if (x[n] < -fontWidth) {
              x[n] = letterEntryXpos;
              char[n] = text.charCodeAt(position) - 97;
