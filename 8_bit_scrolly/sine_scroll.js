41b8bfbd5e1023160a3c59de2ae29b05a5aa48ed
fr1550n <fr1550n@users.noreply.github.com>
2022-07-23 14:09:01 +0100

    '!'

--- 8_bit_scrolly/sine_scroll.js
+++ 8_bit_scrolly/sine_scroll.js
@@ -2,6 +2,8 @@ const text = 'old school sine scroller    another cool javascript tutorial';
 const fontWidth = 54;
 const fontHeight = 71;
 const letters = 12;
+const letterEntryXpos = (letters - 1) * fontWidth;
+
 let x = [];
 let char = [];
 let wiggle = 0;
@@ -11,12 +13,11 @@ let bitmap = new Image();
 bitmap.src = 'font.png';
 let canvas = document.getElementById('myCanvas');
 let context = canvas.getContext('2d');
+
 for (let n = 0; n < letters; n++) {
-    console.log(`n=${n} text.charCodeAt(n)=${text.charCodeAt(n)-97}`);
     char[n] = text.charCodeAt(n) - 97;
     x[n] = n * fontWidth;
-    console.log(`x[n] ${x[n]}`);
-    char[n] = text.charCodeAt(n) - 97;
+    console.log(`n=${n} alphabet character at index: ${text.charCodeAt(n)-97} ${String.fromCharCode(char[n]+97)}`);
 }
 window.requestAnimationFrame(scroll);
 
@@ -26,16 +27,13 @@ function scroll() {
     for (let n = 0; n < letters; n++) {
         console.log(`${new Date().toLocaleString('en-GB', { timeZone: 'UTC' })}: n=${n} position for character: ${String.fromCharCode(char[n]+97)} = ${x[n]}`);	  	  
 	let y = 100; //+ wiggle * Math.sin(n + counter / 6.28);
-	//console.log(`start clipping x: ${char[n] * fontWidth - 97}`);	  
-      
         context.drawImage(bitmap, char[n] * fontWidth, 0, fontWidth, fontHeight, x[n], y, fontWidth, fontHeight);
          x[n]--;
          if (x[n] < -fontWidth) {
-            x[n] = (letters - 1) * fontWidth;
-            //console.log(`same: ${x[n] = (letters - 1) * fontWidth}`); 
-           char[n] = text.charCodeAt(position) - 97;
-           position++;
-           if (position > text.length) position = 0;
+             x[n] = letterEntryXpos;
+             char[n] = text.charCodeAt(position) - 97;
+             position++;
+             if (position > text.length) position = 0;
          }
     }
     if (counter > 200 && wiggle < 30) wiggle = wiggle + .1;
